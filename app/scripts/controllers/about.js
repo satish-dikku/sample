019006f4902a4607eb632beb48ba292807ed0968
unknown <connecttoosatish@gmail.com>
2016-07-30 14:16:10 +0530

    my first email

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstSampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the firstSampleApp
+ */
+angular.module('firstSampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstSampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstSampleApp
+ */
+angular.module('firstSampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
